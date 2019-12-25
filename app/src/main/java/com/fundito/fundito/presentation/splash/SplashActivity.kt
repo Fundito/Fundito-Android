7cad6505b8592f74d154f5cd6c35adc856013d76
MJ Studio <mym0404@gmail.com>
2019-12-25 16:17:09 +0900

    FundingProgressView

--- app/src/main/java/com/fundito/fundito/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/fundito/fundito/presentation/splash/SplashActivity.kt
@@ -1,12 +1,8 @@
 package com.fundito.fundito.presentation.splash
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.fundito.fundito.R
-import com.fundito.fundito.presentation.main.MainActivity
 
 /**
  * Created by mj on 22, December, 2019
@@ -17,9 +13,9 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-        },2000L)
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            startActivity(Intent(this, MainActivity::class.java))
+//        },2000L)
     }
 
 }
