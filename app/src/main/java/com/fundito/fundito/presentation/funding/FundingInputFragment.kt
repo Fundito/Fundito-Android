7918bda0371f936f2762566b48f9f2ed688f87c2
MJ Studio <mym0404@gmail.com>
2019-12-29 18:06:03 +0900

    펀딩 첫번째 페이지

--- app/src/main/java/com/fundito/fundito/presentation/funding/FundingInputFragment.kt
+++ app/src/main/java/com/fundito/fundito/presentation/funding/FundingInputFragment.kt
@@ -1,12 +1,15 @@
 package com.fundito.fundito.presentation.funding
 
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.fundito.fundito.R
+import com.fundito.fundito.common.util.toMoney
+import kotlinx.android.synthetic.main.fragment_funding_input.*
 
 
 /**
@@ -19,6 +22,12 @@ class FundingInputFragment : Fragment() {
         return LayoutInflater.from(context).inflate(R.layout.fragment_funding_input,container,false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        dialView.onInvestmentValueChangedListener = {number->
+            fundinginput_txt.setTextColor(Color.BLACK)
+            fundinginput_txt.text = number.toLong().toMoney()
+        }
+    }
 
     
 }
