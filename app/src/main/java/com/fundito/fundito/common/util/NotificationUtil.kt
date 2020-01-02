cd5f37e809720d1a736f5e0f80a1a4508dd1aa17
MJ Studio <mym0404@gmail.com>
2020-01-03 00:20:51 +0900

    no message

---
+++ app/src/main/java/com/fundito/fundito/common/util/NotificationUtil.kt
@@ -0,0 +1,49 @@
+package com.fundito.fundito.common.util
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import javax.inject.Inject
+
+/**
+ * Created by mj on 02, January, 2020
+ */
+class NotificationUtil @Inject constructor(private val context: Context) {
+
+    companion object {
+        private const val NOTI_CHANNEL_ID = "NOTI_CHANNEL_ID"
+        private const val NOTI_CHANNEL_NAME = "앱 알림"
+
+        private const val NOTI_ID = 100
+    }
+
+    init {
+        initNotificationChannel()
+    }
+
+    private val nm = NotificationManagerCompat.from(context)
+
+
+    private fun initNotificationChannel() {
+
+
+        if(Build.VERSION.SDK_INT >= 26) {
+            val channel = NotificationChannel(NOTI_CHANNEL_ID, NOTI_CHANNEL_NAME,NotificationManager.IMPORTANCE_HIGH)
+            nm.createNotificationChannel(channel)
+        }
+    }
+
+    fun showNotification(message: String) {
+        val noti = NotificationCompat.Builder(context, NOTI_CHANNEL_ID)
+            .setContentTitle("펀디토")
+            .setContentText("하이하이")
+            .build()
+
+        nm.notify(NOTI_ID,noti)
+
+    }
+
+}
---
+++ app/src/main/java/com/fundito/fundito/service/FCMService.kt
@@ -0,0 +1,44 @@
+package com.fundito.fundito.service
+
+import com.facebook.AccessToken
+import com.fundito.fundito.common.util.NotificationUtil
+import com.fundito.fundito.common.util.SPUtil
+import com.fundito.fundito.data.service.NetworkClient
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import timber.log.Timber
+
+/**
+ * Created by mj on 02, January, 2020
+ */
+class FCMService : FirebaseMessagingService() {
+
+    private val notiUtil = NotificationUtil(this)
+
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+
+        Timber.e(p0)
+
+        /**
+         * If signinned, Update FCM Token
+         */
+        val accessToken = AccessToken.getCurrentAccessToken()
+        val isFacebookLogined = accessToken != null && !accessToken.isExpired
+
+        if(!SPUtil.accessToken.isBlank() && isFacebookLogined) {
+            GlobalScope.launch {
+                kotlin.runCatching {
+                    NetworkClient.userService.signIn(accessToken.token,p0)
+                }
+            }
+        }
+    }
+
+    override fun onMessageReceived(p0: RemoteMessage) {
+        Timber.e("hi")
+        notiUtil.showNotification("하이")
+    }
+}
